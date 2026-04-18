7818e81609d040857bc6898e30ad43cc2148d507
yeoheung00 <yeoheung27@gmail.com>
2026-04-18 10:52:36 +0900

    save

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -4,10 +4,10 @@ class Block {
         this.x;
         this.y;
     }
-    generate(random) {
-        this.shape = shape[random];
-        this.x = startPoint[random].x;
-        this.y = startPoint[random].y;
+    generate(type) {
+        this.shape = shape[type];
+        this.x = startPoint[type].x;
+        this.y = startPoint[type].y;
         this.draw();
     }
     move(direction) {
@@ -23,7 +23,7 @@ class Block {
         if (this.moveable(dx, dy, this.shape)) {
             this.x += dx;
             this.y += dy;
-            this.draw();
+            drawField();
         } else if (direction === "ArrowDown") {
             this.fixPosition();
         }
@@ -40,12 +40,12 @@ class Block {
                 temp[x][size - 1 - y] = this.shape[y][x];
             }
         }
-        if(this.adjustable(temp)) this.shape = temp;
+        if (this.adjustable(temp)) this.shape = temp;
         this.draw();
     }
     adjustable(shape) {
         if (shape.length === 3) {
-            const offset = [{dx: 0, dy: 0}, { dx: 1, dy: 0 }, { dx: -1, dy: 0 }, { dx: 0, dy: -1 }];
+            const offset = [{ dx: 0, dy: 0 }, { dx: 1, dy: 0 }, { dx: -1, dy: 0 }, { dx: 0, dy: -1 }];
             for (let i = 0; i < offset.length; i++) {
                 const data = offset[i];
                 if (this.moveable(data.dx, data.dy, shape)) {
@@ -56,7 +56,7 @@ class Block {
             }
         }
         else if (shape.length === 4) {
-            const offset = [{dx: 0, dy: 0}, { dx: 1, dy: 0 }, { dx: 2, dy: 0 }, { dx: -1, dy: 0 }, { dx: -2, dy: 0 }, { dx: 0, dy: -1 }, { dx: 0, dy: -2 }];
+            const offset = [{ dx: 0, dy: 0 }, { dx: 1, dy: 0 }, { dx: 2, dy: 0 }, { dx: -1, dy: 0 }, { dx: -2, dy: 0 }, { dx: 0, dy: -1 }, { dx: 0, dy: -2 }];
             for (let i = 0; i < offset.length; i++) {
                 const data = offset[i];
                 if (this.moveable(data.dx, data.dy, shape)) {
@@ -80,29 +80,38 @@ class Block {
                 field.unshift(Array(width).fill(0));
             }
         });
-        if (field[1].every(cell => cell === 0)) this.generate(Math.floor(Math.random() * 6));
-        else {
+        if (field[1].every(cell => cell === 0)) {
+            bag.shift();
+            while (bag.length !== 3) {
+                const random = Math.floor(Math.random() * 7);
+                if (!bag.includes(random)) bag.push(random);
+            }
+            this.generate(bag[0]);
+
+            drawNext();
+        } else {
             console.log("Game over");
             isStarted = false;
         }
     }
     draw() {
-        drawField();
         this.shape.forEach((line, y) => {
             line.forEach((cell, x) => {
-                ctx.fillStyle = palette[cell];
-                if (cell > 0) ctx.fillRect((this.x + x) * cell_size, (this.y + y) * cell_size, cell_size, cell_size);
+                ctx_field.fillStyle = palette[cell];
+                if (cell > 0) ctx_field.fillRect((this.x + x) * cell_size, (this.y + y) * cell_size, cell_size, cell_size);
             });
         });
     }
 }
 
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+const canvas_field = document.querySelector("#field");
+const ctx_field = canvas_field.getContext("2d");
+const canvas_next = document.querySelector("#next");
+const ctx_next = canvas_next.getContext("2d");
 const width = 10;
 const height = 22;
-const cell_size = canvas.width / width;
-const startPoint = [{ x: 4, y: 0 }, { x: 3, y: 1 }, { x: 3, y: 0 }, { x: 3, y: 0 }, { x: 3, y: 0 }, { x: 3, y: 0 }];
+const cell_size = canvas_field.width / width;
+const startPoint = [{ x: 4, y: 0 }, { x: 3, y: 0 }, { x: 3, y: 0 }, { x: 3, y: 0 }, { x: 3, y: 0 }, { x: 3, y: 0 }, { x: 3, y: 0 }];
 const palette = ["", "#ffff00", "#00ffff", "#ff00ff", "#0000ff", "#ff9900", "#ff0000", "#00ff00"];
 const shape = [[
     [1, 1],
@@ -136,18 +145,45 @@ const shape = [[
 let isStarted = false;
 let blockExist = false;
 let block = new Block();
+let bag = [];
 let INTERVAL = 1000;
 let field = Array.from({ length: height }, () => Array(width).fill(0));
 
 document.addEventListener('keydown', (event) => {
     if (!isStarted) {
         isStarted = true;
-        block.generate(Math.floor(Math.random() * 6));
+        fillBag();
         setTimeout(timerStart, INTERVAL);
         return;
     }
     block.move(event.key);
 });
+
+function fillBag() {
+    let count = 0;
+    for (let i = 0; i < 3; i++) {
+        const random = Math.floor(Math.random() * 7);
+        if (bag.includes(random)) {
+            i--;
+            continue;
+        }
+        bag.push(random);
+    }
+    block.generate(bag[0]);
+}
+
+function drawNext() {
+    ctx_next.clearRect(0, 0, canvas_next.width, canvas_next.height);
+    const offset = [{ x: 1, y: 1 }, { x: 0, y: 0.5 }, { x: 0.5, y: 1 }, { x: 0.5, y: 1 }, { x: 0.5, y: 1 }, { x: 0.5, y: 1 }, { x: 0.5, y: 1 }]
+    shape[bag[1]].forEach((line, y) => {
+        line.forEach((cell, x) => {
+            if (cell > 0) {
+                ctx_next.fillStyle = palette[cell];
+                ctx_next.fillRect(x * cell_size + offset[bag[1]].x * cell_size, y * cell_size + offset[bag[1]].y * cell_size, cell_size, cell_size);
+            }
+        });
+    })
+}
 
 function timerStart() {
     if (!isStarted) return;
@@ -157,20 +193,24 @@ function timerStart() {
 
 
 function drawField() {
-    ctx.clearRect(0, 0, width * cell_size, height * cell_size);
-    ctx.fillStyle = "#c4c4c4";
+    ctx_field.clearRect(0, 0, canvas_field.width, canvas_field.height);
+    //ctx_field.fillStyle = "#c4c4c4";
+    block.draw();
     for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
-            if ((y % 2 === 0 && x % 2 === 0) || (y % 2 === 1 && x % 2 === 1)) {
-                ctx.fillStyle = "#c4c4c4";
-                ctx.fillRect(x * cell_size, y * cell_size, cell_size, cell_size);
-            }
+            //if ((y % 2 === 0 && x % 2 === 0) || (y % 2 === 1 && x % 2 === 1)) {
+            //    ctx_field.fillStyle = "#c4c4c4";
+            //    ctx_field.fillRect(x * cell_size, y * cell_size, cell_size, cell_size);
+            //}
             if (field[y][x] > 0) {
-                ctx.fillStyle = palette[field[y][x]];
-                ctx.fillRect(x * cell_size, y * cell_size, cell_size, cell_size);
+                ctx_field.fillStyle = palette[field[y][x]];
+                ctx_field.fillRect(x * cell_size, y * cell_size, cell_size, cell_size);
+            } else {
+                ctx_field.fillStyle = "#c4c4c4";
+                //ctx_field.fillRect(x * cell_size, y * cell_size, cell_size, cell_size);
             }
         }
     }
-    ctx.fillStyle = "#ff0000";
-    ctx.fillRect(0, cell_size * 2 - 1, width * cell_size, 2);
+    ctx_field.fillStyle = "#ff0000";
+    ctx_field.fillRect(0, cell_size * 2 - 1, width * cell_size, 2);
 }
